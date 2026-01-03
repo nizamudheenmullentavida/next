0d986db52258195a00720649d931a02d534bbeb7
NIZAMUDHEEN <nizamudheenmullentavida1988@gmail.com>
2026-01-03 06:12:59 +0300

    update

--- src/app/products/[productdetails]/page.tsx
+++ src/app/products/[productdetails]/page.tsx
@@ -1,37 +1,71 @@
-import GotoCart from '@/app/components/goToCart/gotocart';
-import { Services } from '@/app/services/productId-services';
-import { title } from 'process';
-import React from 'react'
+// import GotoCart from '@/app/components/goToCart/gotocart';
+// import { Services } from '@/app/services/productId-services';
+// import { title } from 'process';
+// import React from 'react'
 
-  export async function generateMetadata(props:any){
-    var paramstitle = await props.params
-    var producttitles  = paramstitle.productdetails;
-    var producttitle = await Services.getAllProductById(producttitles)
-    return{
-        title:producttitle.title
+//   export async function generateMetadata(props:any){
+//     var paramstitle = await props.params
+//     var producttitles  = paramstitle.productdetails;
+//     var producttitle = await Services.getAllProductById(producttitles)
+//     return{
+//         title:producttitle.title
+//     }
+
+//   }
+
+// export default  async function productDetailspage(props:any) {
+//     // console.log('product details',props)
+    
+//     var params = await props.params
+//     var prodid = params.productdetails;
+
+//      var product = await Services.getAllProductById(prodid)
+
+
+//      var pric = await props.searchParams;
+//      var price = pric.price
+//   return (
+//     <>
+//     <GotoCart/>
+//     <div>
+//       This details page
+//       {product.title}
+//     </div>
+//     <div>{price}</div>
+//     </>
+//   )
+// }
+
+import { Services } from '@/app/services/productId-services';
+import GotoCart from '@/app/components/goToCart/gotocart';
+
+export async function generateMetadata({ params }: any) {
+    const { productdetails } = await params;
+    try {
+        const product = await Services.getAllProductById(productdetails);
+        return { title: product?.title || "Product Not Found" };
+    } catch (e) {
+        return { title: "Error" };
+    }
+}
+
+export default async function ProductDetailsPage({ params, searchParams }: any) {
+    const { productdetails } = await params;
+    const { price } = await searchParams;
+
+    const product = await Services.getAllProductById(productdetails);
+
+    if (!product) {
+        return <div>Product not found</div>;
     }
 
-  }
-
-export default  async function productDetailspage(props:any) {
-    // console.log('product details',props)
-    
-    var params = await props.params
-    var prodid = params.productdetails;
-
-     var product = await Services.getAllProductById(prodid)
-
-
-     var pric = await props.searchParams;
-     var price = pric.price
-  return (
-    <>
-    <GotoCart/>
-    <div>
-      This details page
-      {product.title}
-    </div>
-    <div>{price}</div>
-    </>
-  )
+    return (
+        <>
+            <GotoCart />
+            <div>
+                <h1>{product.title}</h1>
+                <p>Price from URL: {price}</p>
+            </div>
+        </>
+    );
 }
